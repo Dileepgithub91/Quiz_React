f1e886c3786826cf9190a2fe89790d70047fbb71
Dileep kumar <you@example.com>
2023-05-25 05:48:52 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import './App.css';
+import React ,{useState} from 'react';
+import {Data} from './Data';
+import './style.css';
+
+export default function App() {
+  const [showResult,setShowResult]=useState(false);
+  const [currentIndex,setCurrentIndex]=useState(0);
+  const[score,setScore]=useState(0);
+  const currentQuestion=Data[currentIndex];
+  const handleOptions=(item)=>{
+    if(item.isCorrect){
+      setScore(score+1);
+
+    }
+    if(currentIndex<Data.length-1){
+      setCurrentIndex(currentIndex+1);
+
+    }
+    else{
+      setShowResult(true);
+    }
+  };
+  const handleRestart=()=>{
+    setShowResult(false);
+    setCurrentIndex(0);
+    setScore(0)
+  };
+  return(
+    <div className="container">
+      <h2 classname="score">
+        Question {currentIndex +1}/{Data.length}
+      </h2>
+      {showResult ?(
+        <div>
+          <h3 className="score">score{score}</h3>
+          </div>
+      ):(
+        <div className="All">
+          <h3 className="question">
+            <span className="span">
+              {currentIndex+1}.
+            </span>
+            {currentQuestion.question}
+
+          </h3>
+          <div className="options">
+            <ul>
+              {currentQuestion.options.map((item)=>{
+                return(
+                  <li onClick={()=>handleOptions(item)}
+                  key={item.id}>
+                    {item.text}
+                  </li>
+                );
+              })}
+              </ul>
+              <button className="restart" onClick={handleRestart}>Restart</button>
+          </div>
+          </div>
+
+      )}
+      </div>
+
+
   );
 }
 
-export default App;
+
---
+++ src/Data.js
@@ -0,0 +1,53 @@
+export const Data=[{
+    id:1,
+    question:"Which is the Capital of Jharkhand?",
+    options:[
+        {id:1,text:"Ranchi",isCorrect:true},
+
+        { id:2, text:"Hazaribag",isCorrect:false},
+        {
+            id:3,text:"Dhanbad",isCorrect:false
+        },
+        {
+            id:4,text:"Jamshedpur",isCorrect:false
+        },
+    ],
+},
+{
+    id:2,
+    question:"What is the largest planet in our solar system?",
+    options:[
+        {
+            id:1,text:"Mars",isCorrect:false
+
+        },
+        {
+            id:2,text:"Saturn",isCorrect:false
+        },
+        {
+            id:3,text:"Jupiter",isCorrect:true
+        },
+        {
+            id:4,text:"Neptune",isCorrect:false
+        },
+    ],
+},
+{
+    id:3,
+    question:"Which country is known as the Land of the Rising Sun?",
+    options:[
+        {
+            id:1,text:"China",isCorrect:false
+        },
+        {
+            id:2,text:"Japan",isCorrect:true
+        },
+        {
+            id:3,text:"South Korea",isCorrect:false
+        },
+        {
+            id:4,text:"Thailand",isCorrect:false
+        },
+    ],
+},
+];
